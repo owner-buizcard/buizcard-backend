1ba430d6031286d345d29659df8959dbc4150449
dhanasra <dhana.spiderlingz@gmail.com>
2023-11-10 01:09:38 +0530

    update : card apis updated

--- handlers/card.js
+++ handlers/card.js
@@ -12,7 +12,23 @@ const service = require('../services/card');
 const cors_origin = require("../core/cors_origin");
 const { validateAccessToken } = require('../middlewares/authenticate');
 
+const fileUpload = require('express-fileupload');
+
 const app = express();
+
+app.use(fileUpload({
+    limits: { fileSize: 4 * 1024 * 1024 },
+    createParentPath: true,
+    abortOnLimit: true,
+    limitHandler: function (req, res, next) {
+        res.status(413).send({
+            status: "error",
+            message: "The file has exceeded the maximum file limit 4MB",
+            messageCode: "PRO_E001"
+        });
+    },
+    debug: true,
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
