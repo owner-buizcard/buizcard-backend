dcdad4038fc009e02110f7f2931d634f6b679e9a
dhanasra <dhana.spiderlingz@gmail.com>
2023-12-20 10:49:22 +0530

    update: cardlink updated

--- handlers/share.js
+++ handlers/share.js
@@ -11,20 +11,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(open_origin());
 
-app.get('/app/p', (req, res)=>{
+app.get('/app/p/:cardId', (req, res)=>{
 
-    const { userId }= req.query;
+    const { cardId } = req.params;
 
-    const imageUrl = `https://bizcard-dev-v1.s3.amazonaws.com/${userId}/previewImage.jpg`;
-    const redirectUrl = `${process.env.DOMAIN}/app/p?userId=${userId}`;
+    const imageUrl = `https://bizcard-dev-v1.s3.amazonaws.com/${cardId}/previewImage.jpg`;
+    const redirectUrl = `${process.env.DOMAIN}/app/p/${cardId}`;
 
     const metadataHtml = `
         <html>
         <head>
             <meta property="og:image" content="${imageUrl}" />
-            <meta property="og:title" content="Your Web App Title" />
-            <meta property="og:description" content="Description of your web app" />
-            <meta property="og:url" content="https://bizcard-web.web.app/app/cards" />
+            <meta property="og:title" content="Bizcard" />
+            <meta property="og:description" content="Your personalized digital visiting card" />
+            <meta property="og:url" content="${redirectUrl}" />
             <meta property="og:type" content="website" />
         </head>
         <body>
