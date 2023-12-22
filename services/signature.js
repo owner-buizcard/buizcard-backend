bc71dbffaa043dcc431f0a083f0516ff6c2f5c70
dhanasra <dhana.spiderlingz@gmail.com>
2023-12-22 14:46:20 +0530

    update: email-signature updated

--- services/signature.js
+++ services/signature.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const responser = require("../core/responser");
 const { generateQrImage } = require("../core/utils");
 
@@ -12,19 +13,19 @@ async function generateSignature(req, res) {
             qrImageUrl = await generateQrImage(cardId);
         }
 
-        const signature = `
-            <div style="font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5;">
-                <p>${fullName}</p>
-                <p>${jobTitle}</p>
-                <p>${company}</p>
-                <p>${phoneNumber}</p>
-                <p>${location}</p>
-                <p>${disclaimer}</p>
-                ${qrImageUrl ? `<img src="${qrImageUrl}" alt="QR Code">` : ''}
-            </div>
-        `;
+        const templatePath = path.join(rootPath,'templates', 'email_signature_template.html');
+        const htmlTemplate = fs.readFileSync(templatePath, 'utf-8');
 
-        return responser.success(res, signature, "SIGNATURE_S001");
+        const signature = htmlTemplate
+            .replace('[fullName]', `${fullName}`)
+            .replace('[jobTitle]', jobTitle)
+            .replace('[company]', company)
+            .replace('[phoneNumber]', phoneNumber)
+            .replace('[location]', location)
+            .replace('[disclaimer]', disclaimer)
+            .replace('[qrImageUrl]', qrImageUrl);
+
+        return responser.success(res, `${signature}`, "SIGNATURE_S001");
     } catch (error) {
         console.error(error);
         return responser.success(res, null, "GLOBAL_E001");
