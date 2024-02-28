e34b09e7e6588b01ec0d86af7a49b76ebd7e9b93
dhanasra <dhana.spiderlingz@gmail.com>
2024-02-29 00:43:22 +0530

    update: subs

--- services/main.js
+++ services/main.js
@@ -7,13 +7,12 @@ const { generateTokens } = require("./token");
 async function fetchConfigData(req, res) {
     try{
 
-        const [fieldTypes, configs, plans] = await Promise.all([
+        const [fieldTypes, configs] = await Promise.all([
             depManager.CONFIG.getFieldTypesModel().find(),
-            depManager.CONFIG.getConfigModel().find(),
-            depManager.SUBSCRIPTION.getPlanModel().find()
+            depManager.CONFIG.getConfigModel().find()
         ]);
 
-        const config = {fieldTypes, configs, plans};
+        const config = {fieldTypes, configs};
         
         return responser.success(res, {config}, "MAIN_S001")
     }catch(error){
@@ -96,19 +95,53 @@ async function fetchMainData(req, res) {
         ];
         
 
-        const [user, cards, contacts, fieldTypes, configs, plans] = await Promise.all([
+        const [user, cards, contacts, fieldTypes, configs, plans, subscriptions] = await Promise.all([
             depManager.USER.getUserModel().findById(userId),
             depManager.CARD.getCardModel().aggregate(conditionForCards),
             depManager.CONTACT.getContactModel().aggregate(condition),
             depManager.CONFIG.getFieldTypesModel().find(),
             depManager.CONFIG.getConfigModel().find(),
-            depManager.SUBSCRIPTION.getPlanModel().find()
+            depManager.SUBSCRIPTION.getPlanModel().find(),
+            depManager.SUBSCRIPTION.getSubscriptionModel().find({userId}),
         ]);
+
+        const currentDate = new Date();
+
+        const subscriptionMap = {
+            current: [],
+            upcoming: [],
+            expired: []
+        };
+
+        subscriptions.forEach(subscription => {
+            const planData = plans.find((plan )=>{
+                return new mongoose.Types.ObjectId(subscription.plan.id).equals(plan._id)
+            });
+            const duration = subscription.plan.type=="m" ? "1 Month" : "1 Year";
+            const endDate = subscription.endAt;
+            const mappedSubscription = {
+                planId: subscription.plan.id,
+                subscriptionId: subscription._id,
+                name: planData.name,
+                startAt: subscription.startAt,
+                endAt: endDate,
+                duration: duration,
+                type: subscription.plan.type
+            };
+
+            if (subscription.startAt <= currentDate && endDate >= currentDate) {
+                subscriptionMap.current.push(mappedSubscription);
+            } else if (endDate < currentDate) {
+                subscriptionMap.expired.push(mappedSubscription);
+            } else if (subscription.startAt > currentDate) {
+                subscriptionMap.upcoming.push(mappedSubscription);
+            }
+        });
         
         const token = generateTokens(userId)
         const config = {fieldTypes, configs, plans};
         
-        return responser.success(res, {user, contacts, cards, config,  token}, "MAIN_S001")
+        return responser.success(res, {user, contacts, cards, config,  token, subscriptionMap}, "MAIN_S001")
     }catch(error){
         console.log(error);
         return responser.success(res, null, "MAIN_E001")
